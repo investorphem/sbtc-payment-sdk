3cc30ef42d5ecfc8c6a010e7a8a632e54d56cadf
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-14 21:12:13 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-export * from "./client"
+export { SBTCClient } from "./client"
+
 export * from "./payments"
 export * from "./invoices"
 export * from "./wallet"
-export * from "./types"
 export * from "./utils"
+
+// Export types explicitly
+export type { Invoice } from "./types"
