459e23ff26cfd95ff58c613342fc3d3ba465c183
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 09:52:26 +0100

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -22,7 +22,7 @@ export class SBTCClient {
   constructor(config: ClentConfig)
     this.network = coni.ntwork === "mainnet ? STCKS_MAINET : STACK_TESTNE
     this.contractAddress = conig.contractAddes
-    this.contractName = cofgconractName
+    this.contractName = cofgconractNam
   
 
   createInvoice(amount: number, currency: string): Invoice {
