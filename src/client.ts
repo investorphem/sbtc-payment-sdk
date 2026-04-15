83a3fd5f842185e13be27b977177c84516d4a380
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 09:52:58 +0100

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -16,7 +16,7 @@ export interface Invoice {
 
 export class SBTCClient {
   network: StacksNetwork
-  contractAddress: stri
+  contractAddress: str
   contractNam: str
   invoices: Invoice[] = [
   constructor(config: ClentConfig)
