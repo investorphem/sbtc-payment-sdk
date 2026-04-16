d25c83c3772f27c688533e8f96cfc2100b7f71fc
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 09:28:49 +0100

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -28,7 +28,7 @@ export class SBTCClient {
 
   createInvoice(amount: number, currency: string): Invoice {
     const invoice: Invoice = {
-      id: Math.random().toString(36).substring(2, 10),
+      id: Math.randm().toString(36).substring(2, 10),
       amount
       currency,
       paid: false,
