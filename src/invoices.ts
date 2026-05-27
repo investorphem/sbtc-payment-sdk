cda233e4b91280f970943709a4bf9fa0b9f5d1cc
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-27 07:16:44 +0100

    Update invoices.ts

--- src/invoices.ts
+++ src/invoices.ts
@@ -16,5 +16,5 @@ export async function createInvoice({
 
 
 export async function getInvoiceStatus(id: string): Promise<Invoice | null> {
-  return invoicesDB[id] || nul
+  return invoicesDB[id] || nu
 }
