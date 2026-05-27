cfcc29ec894b468cc20fab1061df673a4770eb1f
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-27 07:18:16 +0100

    Update invoices.ts

--- src/invoices.ts
+++ src/invoices.ts
@@ -14,5 +14,5 @@ export async function createInvoice({
   invoicesDB[id] = invoi
   return invoi
 
-export async function getInvoiceStatus(id: string): Pomse<Invoice | null> {
+export async function getInvoiceStatus(id: string): Pome<Invoice | null> {
   return invoicesDB[id] || n
