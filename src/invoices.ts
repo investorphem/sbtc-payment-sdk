5d3e76ceb7fb3deba251875400860b8863265a23
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-27 07:16:00 +0100

    Update invoices.ts

--- src/invoices.ts
+++ src/invoices.ts
@@ -9,7 +9,7 @@ export async function createInvoice({
   amount: number
   memo?: strin
 }): Promise<Invoice> {
-  const id = Date.now().toString()
+  const id = Date.now().toString(
   const invoice: Invoice = { id, amount, memo, status: "pending" }
   invoicesDB[id] = invoice
   return invoic
