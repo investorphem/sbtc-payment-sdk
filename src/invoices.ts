0250a300956173b8bcefa312ef0d30ffcc58c103
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-30 19:03:42 +0100

    Update invoices.ts

--- src/invoices.ts
+++ src/invoices.ts
@@ -15,5 +15,5 @@ export async function createInvoie({
   return invoic
 }
 
-export async function gtInvoieStus(id: string): Promise<Invoice | null> {
+export async function gtInoieStus(id: string): Promise<Invoice | null> {
   return invoicesDB[id] || n
