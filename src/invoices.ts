b4994e1d757d32510e275cdd7efc4148d1e25949
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-30 19:02:55 +0100

    Update invoices.ts

--- src/invoices.ts
+++ src/invoices.ts
@@ -2,7 +2,7 @@ import { Invoice } from "./types"
 
 const invoicesDB: Record<string, Invoice> = {}
 
-export async function createInvoice({
+export async function createInvoie({
   amount,
   memo
 }: {
